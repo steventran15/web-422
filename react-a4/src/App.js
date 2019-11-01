9d303e2ec3836e57b3af348894076c74b04471ad
steventran15 <steventran1534@gmail.com>
2019-11-01 17:19:19 -0400

    part 1 is complete

--- react-a4/src/App.js
+++ react-a4/src/App.js
@@ -8,31 +8,13 @@ import MainContainer from "./components/MainContainer";
 import ProjectsPanel from "./components/ProjectsPanel";
 import EmployeesPanel from "./components/EmployeesPanel";
 import TeamsPanel from "./components/TeamsPanel"; 
+import Overview from "./components/Overview";
 
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <div className="container-fluid">
-          <div className="row">
-            <MainContainer>
-              <h1 className="page-header">Overview</h1>
-              <div className="row">
-                <div className="col-md-4">
-                  <ProjectsPanel/>
-                </div>
-                <div className="col-md-4">
-                  <TeamsPanel/>
-                </div>
-                <div className="col-md-4">
-                  <EmployeesPanel/>
-                </div>
-              </div>
-            </MainContainer>
-          </div>
-        </div>
-      </div>
+      <Overview/>
     );
   }
 }
--- react-a4/src/components/MainContainer.jsx
+++ react-a4/src/components/MainContainer.jsx
@@ -3,6 +3,7 @@ import NavBar from "./NavBar";
 import SideBar from "./SideBar";
 
 class MainContainer extends React.Component {
+  // this component is stateless because the sidebar property is passed down from Overview component, this.props.sidebar not this.state.sidebar. 
   constructor(props) {
     super(props);
     this.state = {
@@ -16,7 +17,7 @@ class MainContainer extends React.Component {
         <NavBar />
         <div className="container-fluid">
           <div className="row">
-            <SideBar highlight={this.state.sidebar} />
+            <SideBar highlight={this.props.sidebar} />
             <div className=" col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
               {this.props.children}
             </div>
