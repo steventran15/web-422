84c93ad111a71ffb6367f94c639045f11816cbd1
Steven Tran <steven.tran15@hotmail.com>
2019-11-01 22:55:54 -0400

    completed notfound.jsx

---
+++ react-a4/src/components/NotFound.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import MainContainer from "./MainContainer";
+
+const NotFound = () => {
+    return (  
+        <div>
+        <div className="container-fluid">
+          <div className="row">
+            <MainContainer>
+              <h1 className="page-header"><b>Not Found</b></h1>
+                    <span>Page Not Found</span>
+            </MainContainer>
+          </div>
+        </div>
+      </div>
+
+        
+    );
+}
+ 
+export default NotFound;
